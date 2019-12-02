74376e15f7570b6ced441e128f4c1f9b8a737149
Jingchao Chen <jcc8887@gmail.com>
2019-12-01 22:38:41 -0800

    Add Tic Tac Toe example from Redux

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
-import "../css/App.css"
+import React, { Component } from 'react';
+import '../css/App.css';
+import Game from './components/Game';
 
 class App extends Component {
 	render() {
 		return(
 			<div className="App">
-				<h1> Hello, World! </h1>
+				<h1>Tic Tac Toe</h1>
+				<Game />
 			</div>
 		)
 	}
---
+++ client/src/js/components/Board.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Square from './Square';
+
+class Board extends Component {
+  renderSqaure(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSqaure(0)}
+          {this.renderSqaure(1)}
+          {this.renderSqaure(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSqaure(3)}
+          {this.renderSqaure(4)}
+          {this.renderSqaure(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSqaure(6)}
+          {this.renderSqaure(7)}
+          {this.renderSqaure(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
